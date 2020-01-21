646e6c6bb7835cc7d7c82a76a2a0f96ce410f27b
Olena Starchak <58229959+olenastarchak@users.noreply.github.com>
2020-01-21 09:28:43 +0200

    minor changes

--- FL12_HW8/homework/js/pipe.js
+++ FL12_HW8/homework/js/pipe.js
@@ -1,6 +1,6 @@
 let addOne = x => x + 1;
 function pipe(var1, ...callback) {
-  for (let element of [...callback]) {
+  for (let element of callback) {
     var1 = element(var1);
   }
   return var1;
