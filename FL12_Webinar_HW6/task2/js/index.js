2ce547d6d5fd302f908edff24e4476e50b755e68
Olena Starchak <58229959+olenastarchak@users.noreply.github.com>
2020-03-03 21:56:02 +0200

    Webinar HW6 done

--- FL12_Webinar_HW6/task2/js/index.js
+++ FL12_Webinar_HW6/task2/js/index.js
@@ -1,31 +1,108 @@
-const list = $('.list');
-const input = $('#add-input');
-const add = $('#add-submit');
+$.fn.todolist = () => {
+  const list = $(".list");
+  const input = $("#add-input");
+  const add = $("#add-submit");
 
-const todos = [
-  {
-    text: 'Buy milk',
-    done: false
-  },
-  {
-    text: 'Play with dog',
-    done: true
+  const todos = [
+    {
+      text: "Buy milk",
+      done: false
+    },
+    {
+      text: "Play with dog",
+      done: true
+    }
+  ];
+
+  let items;
+  if (getFromLocal("myStorage").length === 0) {
+    items = todos;
+  } else {
+    items = getFromLocal("myStorage");
   }
-];
+  loadList(items);
 
-add.click(function() {
-  todos.push({
-    text: input.val(),
-    done: true
-  })
-  input.val('');
-})
+  add.click(function(e) {
+    e.preventDefault();
+    if (input.val().trim()) {
+      items.push({
+        text: input.val(),
+        done: false
+      });
+    }
+    input.val("");
+    loadList(items);
+    storeData("myStorage", items);
+  });
 
-function loadList(todos) {
-  $('li').remove();
-  for(let i=0; i<todos.length; i++) {
-    $('ul').append(`<li class='item'><span class='item-text'>${todos[i].text}</span><button class='item-remove'>Remove</button></li>`)
+  function loadList(items) {
+    $("li").remove();
+    for (let i = 0; i < items.length; i++) {
+      if (!items[i].done) {
+        $("ul").append(`<li class='item'><span class='item-text'>${items[i].text}</span><button class='item-remove'>Remove</button></li>`);
+      } else {
+        $("ul").append(`<li class='item'><span class='item-text done'>${items[i].text}</span><button class='item-remove'>Remove</button></li>`);
+      }
+    }
   }
+
+  function storeData(key, obj) {
+    localStorage[key] = JSON.stringify(obj);
+  }
+
+  function getFromLocal(key) {
+    if (localStorage[key]) {
+      return JSON.parse(localStorage[key]);
+    } else {
+      return [];
+    }
+  }
+
+  list
+    .on("click", ".item-remove", function() {
+      const index = $(this).parent().index();
+      $(this).parent().remove();
+      items.splice(index, 1);
+      storeData("myStorage", items);
+    })
+    .on("click", ".item-text", function() {
+      $(this).toggleClass("done");
+      const thisText = $(this).text();
+      const index = items.findIndex(todo => todo.text === thisText);
+      if (items[index].done) {
+        items[index].done = false;
+      } else {
+        items[index].done = true;
+      }
+      storeData("myStorage", items);
+    });
+
+  $("#search-submit").click(function(e) {
+    e.preventDefault();
+    let delay = 100;
+    let delayEnd = 200;
+    for (let i = 0; i < items.length; i++) {
+      if ($("#search-input").val() === items[i].text) {
+        setTimeout(() => {
+          $($(".item-text")[i]).css("backgroundColor", "lightgreen");
+        }, delay);
+        delayEnd += 2000;
+        setTimeout(function() {
+          $($(".item-text")[i]).css("backgroundColor", "white");
+        }, delayEnd);
+        break;
+      } else {
+        setTimeout(() => {
+          $($(".item-text")[i]).css("background-color", "pink");
+        }, delay);
+        delayEnd += 65;
+        setTimeout(function() {
+          $($(".item-text")[i]).css("background-color", "white");
+        }, delayEnd);
+        delay += 40;
+      }
+    }
+    $("#search-input").val("");
+  });
 }
-
-loadList(todos);
+$().todolist();
